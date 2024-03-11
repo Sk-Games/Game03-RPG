5e19fe98aef7fbc239cc2516e210085f62822e82
Shwetank Singh <ssingh.21.2019@gmail.com>
2024-03-11 18:57:02 +0530

    upload16

--- Assets/Game/scripts/Combat/Projectile.cs
+++ Assets/Game/scripts/Combat/Projectile.cs
@@ -9,13 +9,24 @@ namespace RPG.Combat
     public class Projectile : MonoBehaviour
     {
         [SerializeField] float speed = 1f;
+        [SerializeField] bool isHoming = true;
+        [SerializeField] GameObject hitEffect = null;
         Health target = null;
         float damage = 0;
+
+        private void Start()
+        {
+            transform.LookAt(GetAimLocation());
+        }
 
         private void Update()
         {
             if (target == null) return;
-            transform.LookAt(GetAimLocation());
+            if (isHoming && !target.IsDead())
+            {
+                transform.LookAt(GetAimLocation());
+            }
+            
             transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
 
@@ -38,7 +49,12 @@ namespace RPG.Combat
         private void OnTriggerEnter(Collider other)
         {
             if (other.GetComponent<Health>()!=target) { return; }
+            if (target.IsDead()) { return; }
             target.TakeDamage(damage);
+            if(hitEffect!= null)
+            {
+                Instantiate(hitEffect, GetAimLocation(), transform.rotation);
+            }
             Destroy(gameObject);
         }
     }
